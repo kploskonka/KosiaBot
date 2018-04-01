b0125d891957a0d73d27fbc953fb872cd6eacf9e
kaska0505 <kasplosk@gmail.com>
2018-04-01 21:10:45 +0200

    Update randomcat.js

--- commands/randomcat.js
+++ commands/randomcat.js
@@ -4,12 +4,21 @@ module.exports = {
     name: 'randomcat',
     description: 'Wyświetla losowego kota!',
     aliases: ['kot', 'meow', 'rcat', 'rkot', 'losowykot'],
+    cooldown: 10,
     execute(message, args) {
     	const url = 'http://aws.random.cat/meow';
 
     	request.get(url, (error, response, body) => {
-    		let json = JSON.parse(body);
-    		message.channel.send(`${json.file}`);
+    		try {
+    			let json = JSON.parse(body);
+    			message.channel.send(`${json.file}`);
+    		}
+    		catch (error) {
+    			console.log(error);
+    			if(error.name == "SyntaxError") {
+    				message.channel.send('Wystąpiły problemy z połączeniem z API! Spróbuj ponownie.');
+    			}
+    		}
     	})
     },
 };
