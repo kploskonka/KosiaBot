24cb765de9fa33c755b0b96b13df604607e7565b
kaska0505 <kasplosk@gmail.com>
2018-04-01 20:50:05 +0200

    Connected to random.cat API

--- commands/randomcat.js
+++ commands/randomcat.js
@@ -1,8 +1,15 @@
+const request = require('request');
+
 module.exports = {
     name: 'randomcat',
     description: 'Wyświetla losowego kota!',
     aliases: ['kot', 'meow', 'rcat', 'rkot', 'losowykot'],
     execute(message, args) {
-    	//TODO
+    	const url = 'http://aws.random.cat/meow';
+
+    	request.get(url, (error, response, body) => {
+    		let json = JSON.parse(body);
+    		message.channel.send(`${json.file}`);
+    	})
     },
 };
